0f41ae286e9921616033a235de59059d4ed0610f
Kevin Aumiller <kaumill1@jhu.edu>
2019-08-26 18:30:17 -0400

    completed homework 1 exercise 2

---
+++ day1-homework/day1-exercise-2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+f = open("SRR072893.sam")
+counter = 0
+
+for i, line in enumerate(f):
+    fields = line.split("\t")
+    if line.startswith("@"):
+         continue
+    if fields[2] == "*":
+         continue
+    if "NM:i:0" in line:
+        counter += 1
+
+print(counter)
+
+
+#NM:i:0
