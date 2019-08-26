3bc8ae14ca56df63ebe495a75d9e989ed2e61f62
Kevin Aumiller <kaumill1@jhu.edu>
2019-08-26 19:00:28 -0400

    completed homework 1 exercise 4

---
+++ day1-homework/day1-exercise-4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+f = open("SRR072893.sam")
+counter = 0
+
+for i, line in enumerate(f):
+    fields = line.split("\t")
+    if line.startswith("@"):
+         continue
+    if fields[2] == "*":
+         continue
+    if i > 10:
+        break
+    
+    print(fields[2])
