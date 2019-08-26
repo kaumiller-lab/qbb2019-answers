262063bde5bdc00e96ba83f91d28f2e3ca50b240
Kevin Aumiller <kaumill1@jhu.edu>
2019-08-26 18:01:27 -0400

    corrected homework 1 exercise 1

--- day1-homework/day1-exercise-1.py
+++ day1-homework/day1-exercise-1.py
@@ -4,6 +4,11 @@ f = open("SRR072893.sam")
 counter = 0
 
 for i, line in enumerate(f):
+    fields = line.split("\t")
+    if line.startswith("@"):
+         continue
+    if fields[2] == "*":
+         continue
     counter += 1
 
 print(counter)
