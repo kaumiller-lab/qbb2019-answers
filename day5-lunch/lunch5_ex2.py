f3f0c149a4f838a24a6cde248dacc0a39faabb32
Kevin Aumiller <kaumill1@jhu.edu>
2019-08-30 14:05:05 -0400

    lunch 5 exercise 2

---
+++ day5-lunch/lunch5_ex2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ctab = sys.argv[1]
+
+df = pd.read_csv(sys.argv[1])
+
+col_names = df.columns.values.tolist()
+
+
+for i, line in enumerate(open(ctab)):
+    if i == 0:
+        continue
+    fields = line.rstrip("\n").split("\t")
+    
+    if fields[2] == "+":
+        prmtr_l = int(fields[3]) - 500
+        prmtr_r = int(fields[3]) + 500
+        
+    else:
+        prmtr_l = int(fields[4]) - 500
+        prmtr_r = int(fields[4]) + 500
+        
+    if prmtr_l < 0:
+        prmtr_l = 0
+        
+    print(fields[1] , str(prmtr_l), str(prmtr_r), fields[5], sep = "\t")
+        
+        
+
+
+        
+        
+    
+  
+    
+    
